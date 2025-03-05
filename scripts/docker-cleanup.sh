da31c3efc9019695e498091e32230b8a14eb1731
Alessio <giobbia82@gmail.com>
2025-03-05 15:56:02 +0100

    Update docker-cleanup.sh

--- scripts/docker-cleanup.sh
+++ scripts/docker-cleanup.sh
@@ -2,7 +2,7 @@
 
 # Stop all running containers
 echo "Stopping all containers..."
-docker stop $(docker ps -a -q)
+docker stop $(docker ps -q)
 
 # Remove all containers
 echo "Removing all containers..."
@@ -14,12 +14,10 @@ docker rmi -f $(docker images -a -q)
 
 # Remove all volumes
 echo "Removing all volumes..."
-# docker volume rm $(docker volume ls -q)
 docker volume prune -f
 
 # Remove all networks
 echo "Removing all networks..."
-# docker network rm `docker network ls -q`
 docker network prune -f
 
 echo "Docker cleanup complete."
